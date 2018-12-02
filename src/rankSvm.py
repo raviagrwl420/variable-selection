ad250176fa5f04a41d3b22835dc97856e2b0a54b
cabraham <christopher_abraham@sfu.ca>
2018-12-02 14:15:15 -0800

    Update rankSvm.py

--- src/rankSvm.py
+++ src/rankSvm.py
@@ -1,15 +1,15 @@
-from sklearn import svm, linear_model
-from sklearn.model_selection import KFold
-from sklearn.svm import SVC
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.svm import SVR
+
 
 
 
 class svm(object):
     def rank(X,y):
-        kf = KFold(n_splits=len(X))
+        kf = KFold(n_splits=len(X[1]))
         for train, test in kf.split(X):
             print("%s %s" % (train, test))
         X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
-        clf = SVC(gamma='auto')
+        clf = SVR(gamma='auto')
         clf.fit(X_train, y_train)
-        return clf.score(X_test, y_test))
+        return clf.score(X_test, y_test)
