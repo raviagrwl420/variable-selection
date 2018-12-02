837ed709f9a8726072e059172ee7e29a4ae3dbc5
cabraham <christopher_abraham@sfu.ca>
2018-12-02 13:48:47 -0800

    Add new file

---
+++ src/rankSvm.py
@@ -0,0 +1,21 @@
+import itertools
+import numpy as np
+from scipy import stats
+import pylab as pl
+from sklearn import svm, linear_model
+from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+
+
+
+class rankSvm(object):
+    def svmRank(X,y):
+        kf = KFold(n_splits=len(X))
+        for train, test in kf.split(X):
+            print("%s %s" % (train, test))
+        X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+        clf = SVC(gamma='auto')
+        clf.fit(X_train, y_train)
+        return clf.score(X_test, y_test))
