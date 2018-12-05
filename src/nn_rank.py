bdda248b5c01c0f0257b1d47b499d5f6981c9400
Ravi Agrawal <ravi.agrwl420@gmail.com>
2018-12-04 17:18:41 -0800

    Add basic code for NN Rank

---
+++ src/nn_rank.py
@@ -0,0 +1,55 @@
+import torch.nn as nn
+import torch.optim as optim
+
+class NNRank(nn.Module):
+	"""
+	A simple 2 layer neural network for ranking.
+	"""
+
+	def __init__(self, n_in, n_hidden, n_out, bias=True):
+		super(NNRank, self).__init__()
+
+		self.n_in = n_in
+		self.n_hidden = n_hidden
+
+		self.fc1 = nn.Linear(n_in, n_hidden)
+		self.dropout = nn.Dropout(p=0.5)
+		self.fc2 = nn.Linear(n_hidden, n_out)
+
+
+		self.model = nn.Sequential(self.fc1, nn.ReLU(), self.dropout, self.fc2, nn.Sigmoid())
+
+	def forward(self, inputs):
+		return self.model(inputs)
+
+	def train(self, inputs, targets):
+		# Initialize criterion as MarginRankingLoss 
+		criterion = nn.MarginRankingLoss()
+
+		# Initialize optimizer
+		optimizer = optim.SGD(list(self.fc1.parameters()) + list(self.fc2.parameters()),
+			lr=0.001, momentum=0.9)
+
+		# Select all the items with target class 1 i.e good variables
+		item1 = torch.index_select(targets==1)
+		# Select all the items with target class 0 i.e. bad variables
+		item2 = torch.index_select(targets==0)
+		# Use all combinations of item1 and item2 along with the corresponding targets
+		modified_targets = # TODO: Compute!!
+
+		for epoch in range(NUM_EPOCH):
+			# Zero the parameter gradients
+			optimizer.zero_grad()
+
+			# Forward + backward + optimize
+			outputs = self.forward(inputs)
+
+			# item1 is the first item from the pair
+			# item2 is the second item from the pair
+			# target is either +1 or -1 depending on the ordering of item1 and item2
+			loss = criterion(item1, item2, modified_targets)
+
+			loss.backward()
+			optimizer.step()
+
+			print('[%d] loss: %.3f' % (epoch + 1, loss.item()))
